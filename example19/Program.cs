fb3438399e15b454c31f68aa4b3fc1ce7cc01e8f
SergeyMitroshin <sergey@host-best.com>
2022-11-22 22:49:04 +0300

    ex19

--- example19/Program.cs
+++ example19/Program.cs
@@ -9,3 +9,22 @@
 
 23432 -> да
 */
+
+Console.WriteLine("Введите число");
+string str = Console.ReadLine();
+int i=0;
+int j= str.Length-1;
+bool result = false;
+while (i<j)
+    {
+    if (str[i] != str[j])
+        { result = false;
+        break;}
+    result=true;
+    i++;
+    j--; }
+if (result == false) Console.WriteLine ("нет");
+    else Console.WriteLine("да");
+
+
+
