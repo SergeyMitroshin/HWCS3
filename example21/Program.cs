4610ed41f1a0b8fd3a715fd6e92c2f5693a5bde6
SergeyMitroshin <sergey@host-best.com>
2022-11-22 23:27:20 +0300

    ex21

--- example21/Program.cs
+++ example21/Program.cs
@@ -11,4 +11,23 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+Console.WriteLine("Введите координату x1");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату y1");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату z1");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату x2");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату y2");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату z2");
+int z2 = int.Parse(Console.ReadLine());
+
+double result =  Math.Sqrt(Math.Pow((x2-x1),2)+ 
+                          Math.Pow((y2-y1),2)+
+                          Math.Pow ((z2-z1),2));
+Console.WriteLine(result);
